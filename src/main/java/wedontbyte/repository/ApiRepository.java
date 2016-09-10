225e6110fe63bcf822fdc317887458b9c3a22074
mmckinless <mmckinless@hotmail.co.uk>
2016-09-10 11:35:19 +0100

    Addd into repository

--- src/main/java/wedontbyte/repository/ApiRepository.java
+++ src/main/java/wedontbyte/repository/ApiRepository.java
@@ -1,6 +1,8 @@
 package wedontbyte.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import wedontbyte.repository.rowmapper.IssueDtoRowMapper;
 import wedontbyte.service.ApiRepositoryService;
 
 /**
@@ -8,4 +10,12 @@ import wedontbyte.service.ApiRepositoryService;
  */
 @Repository
 public class ApiRepository implements ApiRepositoryService {
+
+    private IssueDtoRowMapper issueDtoRowMapper;
+
+    @Autowired
+    public ApiRepository() {
+        issueDtoRowMapper = new IssueDtoRowMapper();
+    }
+
 }
