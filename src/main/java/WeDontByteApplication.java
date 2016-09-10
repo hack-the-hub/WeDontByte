a309b7d2ee5bea2b161c5db44f35170ff3d1b2bd
pixelheartshackathon <pixelhearts13@gmail.com>
2016-09-10 11:02:49 +0100

    Setting up

--- src/main/java/WeDontByteApplication.java
+++ src/main/java/WeDontByteApplication.java
@@ -1,5 +1,15 @@
-/**
- * Created by mark on 10/09/2016.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration
 public class WeDontByteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeDontByteApplication.class, args);
+    }
+
+
 }
